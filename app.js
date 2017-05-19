578a4e2d8d22e3ca69e3c796e40a59916a84e8a5
muirurikin <muirurikin@gmail.com>
2017-05-19 20:19:59 +0300

    Added routes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var routes = require('./routes/index');
 
 var app = express();
 
@@ -10,6 +11,9 @@ app.set('view engine', 'ejs');
 
 //Static Files
 app.use(express.static('./public'));
+
+//serve routes
+routes(app);
 
 //set port
 app.listen(3000,(console.log('Listening on port 3000')));
